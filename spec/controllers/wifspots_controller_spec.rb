a8eecbe7a2e6d001f2dd5e951b169b6d69952b68
RAYNOLD <raynold.sim@gmail.com>
2016-07-18 08:56:06 -0700

    test

--- spec/controllers/wifspots_controller_spec.rb
+++ spec/controllers/wifspots_controller_spec.rb
@@ -4,18 +4,6 @@ require 'spec_helper'
 
 describe Api::V1::WifispotsController do
 	render_views
-	describe 'GET search' do
-		it "should enter valid search and get json result" do
-			get 'search', {:lang => 'en', :s => 'shinjuku', :format => 'json'}
-			expect(response.content_type).to eq("application/json")
-			p response.body	
-		end
-		it "should enter a count of 10 and get 10 results" do
-			get 'search', {:lang => 'en', :s => 'shinjuku', :count => "10"}
-			expect(response.content_type).to eq("application/json")
-			
-		end
-	end
 
 	describe 'GET gps' do
 		it "should enter valid x and y and get json result" do
